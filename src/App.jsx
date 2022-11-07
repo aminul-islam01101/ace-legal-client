d69e894d5b7205656bee09cc23a96a4d7090eab6
aminulislam01101 <aminul.islam01101@gmail.com>
2022-11-07 21:38:27 +0600

    First commit-Folder setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+    console.log('app');
+
+    return <div>hello</div>;
+};
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const daisyPlugin = require('daisyui');
+const Form = require('@tailwindcss/forms');
+
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            width: {
+                95: '95%',
+                90: '90%',
+                85: '85%',
+                80: '80%',
+                70: '70%',
+                60: '60%',
+                40: '40%',
+            },
+            maxWidth: {
+                95: '95%',
+                90: '90%',
+                85: '85%',
+                80: '80%',
+                70: '70%',
+                60: '60%',
+                40: '40%',
+            },
+        },
+    },
+    plugins: [daisyPlugin, Form],
+    daisyui: {
+        styled: true,
+        themes: false,
+        base: false,
+        utils: true,
+        logs: true,
+        rtl: false,
+        prefix: '',
+        darkTheme: 'dark',
+    },
+};
